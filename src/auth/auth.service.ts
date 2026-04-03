22e2b00c9e84d1cfd00963e8cdb8ee27c81c6b82
Rafi A Syifaa <mrafiasyifaa@gmail.com>
2026-04-03 09:29:07 +0700

    feat: jwt & auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt'
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService,
+    ){}
+
+    async login(email: string, password: string): Promise<string>{
+        const user = await this.usersService.findByEmail(email)
+
+        const isValid = user && (await bcrypt.compare(password, user.password))
+
+        if(!isValid){
+            throw new UnauthorizedException('Email atau password salah!');
+        }
+
+        return this.jwtService.sign({ sub: user.id, email: user.email})
+    }
+}
