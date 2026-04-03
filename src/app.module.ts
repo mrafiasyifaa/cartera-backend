0628a4dde717505bce87f2f600a6f63a149c0ae0
Rafi A Syifaa <mrafiasyifaa@gmail.com>
2026-04-03 08:41:53 +0700

    feat: root app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { HealthModule } from './health/health.module';
-import { MetricsModule } from './metrics/metrics.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ThrottlerModule } from '@nestjs/throttler'
 
 @Module({
-  imports: [AuthModule, UsersModule, HealthModule, MetricsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports:[
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => ({
+      type: 'mysql',
+      host: config.get<string>('DATABASE_HOST'),
+      port: parseInt(config.get<string>('DATABASE_PORT') || '3306', 10),
+      username: config.get<string>('DATABASE_USER'),
+      password: config.get<string>('DATABASE_PASS'),
+      database: config.get<string>('DATABASE_NAME'),
+      autoLoadEntities: true,
+      synchronize: true, //Ubah waktu prod
+    }),
+  }),
+  ThrottlerModule.forRoot([
+    {
+    ttl: 60000,
+    limit: 5,
+    },
+  ]),
+],
 })
 export class AppModule {}
