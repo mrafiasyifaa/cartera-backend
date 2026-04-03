38a7dbf470ac414708e2119aa7324fb214abd3da
Rafi A Syifaa <mrafiasyifaa@gmail.com>
2026-04-03 09:59:30 +0700

    feat: auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from '../users/users.module';
+import type {StringValue} from 'ms'
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET') || '',
+        signOptions: {expiresIn: (config.get<string>('JWT_EXPIRES_IN') || '1d') as StringValue},
+      })
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
