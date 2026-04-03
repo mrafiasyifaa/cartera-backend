93febdab841e6384da6812cc645411efbdc80774
Rafi A Syifaa <mrafiasyifaa@gmail.com>
2026-04-03 09:19:52 +0700

    feat: users.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly usersRepository: Repository<User>,
+    ) {}
+
+    async findByEmail(email: string): Promise<User | null>{
+        return this.usersRepository.findOne({ where: { email } });
+    }
+}
