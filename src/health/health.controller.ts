648ed872aa84381cc64a94c498125798fd85c08a
Rafi A Syifaa <mrafiasyifaa@gmail.com>
2026-04-05 14:54:26 +0700

    feat: health for monitoring API

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheck, HealthCheckService, TypeOrmHealthIndicator } from '@nestjs/terminus';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('health')
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  @ApiOperation({ summary: 'Cek status aplikasi dan database' })
+  check() {
+    return this.health.check([
+      () => this.db.pingCheck('database'),
+    ]);
+  }
+}
