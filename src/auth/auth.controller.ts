f0ecdaa88faf439f359483217de34f219d09cae0
Rafi A Syifaa <mrafiasyifaa@gmail.com>
2026-04-06 13:04:54 +0700

    feat: Updating auth.controller for JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post, Res, HttpCode } from '@nestjs/common';
-import type { Response } from 'express';
+import { Body, Controller, Post, Get, Res, HttpCode, UseGuards, Req } from '@nestjs/common';
+import type { Response, Request } from 'express';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { RegisterDto } from './dto/register.dto';
+import { JwtAuthGuard } from './jwt.guard';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -13,25 +14,27 @@ export class AuthController {
   @Post('login')
   @HttpCode(200)
   @ApiOperation({ summary: 'Login dan set JWT cookie' })
-  async login(
-    @Body() dto: LoginDto,
-    @Res({ passthrough: true }) res: Response,
-  ) {
+  async login(@Body() dto: LoginDto, @Res({ passthrough: true }) res: Response) {
     const token = await this.authService.login(dto.email, dto.password);
-
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
       maxAge: 24 * 60 * 60 * 1000,
     });
-
     return { message: 'Login berhasil' };
   }
 
   @Post('register')
-  @ApiOperation({ summary: 'Register user baru' })
-  async register(@Body() dto: RegisterDto){
-    return this.authService.register(dto.email, dto.password)
+  @ApiOperation({ summary: 'Registrasi user baru' })
+  async register(@Body() dto: RegisterDto) {
+    return this.authService.register(dto.email, dto.password);
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Cek user yang sedang login' })
+  getMe(@Req() req: Request) {
+    return req.user;
   }
 }
