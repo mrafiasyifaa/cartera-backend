9afedc9678b4b3e3ad8b64053357ac24927532af
Rafi A Syifaa <mrafiasyifaa@gmail.com>
2026-04-06 08:41:38 +0700

    feat: Adding register to usersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity'
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -12,5 +13,16 @@ export class UsersService {
 
     async findByEmail(email: string): Promise<User | null>{
         return this.usersRepository.findOne({ where: { email } });
+    }
+
+    async create(email: string, password: string): Promise<User>{
+        const existing = await this.findByEmail(email);
+        if(existing){
+            throw new ConflictException('Email sudah terdaftar')
+        }
+
+        const hash = await bcrypt.hash(password, 12);
+        const user = this.usersRepository.create({ email, password: hash });
+        return this.usersRepository.save(user);
     }
 }
